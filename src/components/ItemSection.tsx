360640276e6ddad8863e5702a4ae32d861b043a9
Icer Liang <liangshuang@sutu.tech>
2025-04-04 21:10:58 +0800

    construction ribbon for items

--- src/components/ItemSection.tsx
+++ src/components/ItemSection.tsx
@@ -5,6 +5,7 @@ export interface Item {
   title: string;
   description?: string;
   image?: string;
+  construction?: boolean;
 }
 
 interface ItemSectionProps {
@@ -23,8 +24,20 @@ const ItemSection: React.FC<ItemSectionProps> = ({ title, items }) => {
           <a
             key={index}
             href={`/${item.id}`}
-            className="block p-4 hover:text-blue-600 transition-colors text-gray-700 bg-white rounded-md shadow-sm border border-gray-100 hover:shadow-md hover:border-gray-200 transition-all"
+            className="block p-4 hover:text-blue-600 transition-colors text-gray-700 bg-white rounded-md shadow-sm border border-gray-100 hover:shadow-md hover:border-gray-200 transition-all relative"
           >
+            {item.construction && (
+              <div className="absolute top-2 right-5 transform translate-x-1/3 -translate-y-1/3 z-10">
+                <div className="bg-yellow-400 text-xs font-bold px-2 py-1 rounded-full shadow-md flex items-center">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 64 64">
+                    <path fill="currentColor" d="M61.063 17h-8.438v-2.148a6.564 6.564 0 1 0-3.75 0V17h-33.75v-2.148A6.564 6.564 0 0 0 13.25 2a6.562 6.562 0 0 0-1.874 12.852V17H2.938a.937.937 0 0 0-.938.938v18.75c0 .519.419.938.938.938h8.438v6.563H7.938v3.75h3.437v10.752C11.375 59.53 9.5 62 9.5 62H17s-1.875-2.47-1.875-3.311V47.938h33.75V58.69C48.875 59.53 47 62 47 62h7.5s-1.875-2.47-1.875-3.311V47.938h3.437v-3.75h-3.437v-6.563h8.438a.937.937 0 0 0 .938-.938v-18.75a.94.94 0 0 0-.938-.937m-20.704 1.875h9.907L43.292 35.75h-9.905zM8.563 8.563c0-2.587 2.099-4.688 4.688-4.688s4.688 2.101 4.688 4.688a4.688 4.688 0 0 1-9.376 0M4.145 35.468a.93.93 0 0 1-.27-.655v-15c0-.519.419-.938.938-.938h6.579zm9.105 11.283a.688.688 0 1 1 0-1.376a.688.688 0 0 1 0 1.376m.053-11.001l7.372-16.875h9.94L23.641 35.75zm35.572 8.438h-33.75v-6.563h33.75zm1.875 2.563a.688.688 0 1 1 0-1.376a.688.688 0 0 1 0 1.376m0-33.501a4.688 4.688 0 1 1 0-9.376a4.688 4.688 0 0 1 0 9.376m9.375 21.563a.937.937 0 0 1-.938.938h-6.149l6.842-16.557a.93.93 0 0 1 .245.619z"/>
+                  </svg>
+                  <span className='ml-1'>
+                    WIP
+                  </span>
+                </div>
+              </div>
+            )}
             <div className="flex items-start gap-4">
               {item.image && (
                 <img 
