4e5ea26f66941351228e3ce8fa55df2a8ea5fcda
Icer Liang <liangshuang@sutu.tech>
2025-04-09 08:29:10 +0800

    update event list

--- src/data/list.ts
+++ src/data/list.ts
@@ -9,7 +9,16 @@ export interface ResourceLink {
   description: string;
   link: string;
   recommended?: boolean;
-  outdated?: string;
+}
+
+export interface EventLink {
+  title: string;
+  description?: string;
+  link: string;
+  recommended?: boolean;
+  outdated: string;
+  location: string;
+  dateDescription: string;
 }
 
 export interface ResourceSection {
@@ -17,7 +26,7 @@ export interface ResourceSection {
   title: string;
   description?: string;
   emoji: string;
-  links: ResourceLink[];
+  links: (ResourceLink | EventLink)[];
 }
 
 export interface Resource {
