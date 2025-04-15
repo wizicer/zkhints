12e2640cdc9c9cb73ebbddc12fc9db3911b42d45
Icer Liang <liangshuang@sutu.tech>
2025-04-15 09:23:49 +0800

    add link

--- src/data/list/beginner.ts
+++ src/data/list/beginner.ts
@@ -243,5 +243,18 @@ export const beginner: Resource = {
         },
       ],
     },
+    {
+      id: "visions",
+      title: "Visionary Insights",
+      description: "Insights about the future of ZK.",
+      emoji: "🔮",
+      links: [
+        {
+          title: "The ZK Endgame",
+          description: "Where are we going and what should we be optimizing for?",
+          link: "https://blog.zkcloud.com/p/the-zk-endgame",
+        },
+      ],
+    },
   ],
 };
