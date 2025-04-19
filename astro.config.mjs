8dd1254eb9084b95312d5737effccc6aa0858eb9
Icer Liang <liangshuang@sutu.tech>
2025-04-19 22:05:12 +0800

    enable katex in markdown and tailwind typography for markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  markdown: {
+    syntaxHighlight: "prism",
+    rehypePlugins: [rehypeKatex],
+    remarkPlugins: [remarkMath],
+  },
 });
