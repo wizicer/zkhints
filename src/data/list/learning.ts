dfaa0593aa7f51142c1a44c930b5fe356df97045
Icer Liang <liangshuang@sutu.tech>
2025-04-30 16:05:37 +0800

    update links

--- src/data/list/learning.ts
+++ src/data/list/learning.ts
@@ -531,5 +531,28 @@ export const learning: Resource = {
         },
       ],
     },
+    {
+      id: "io",
+      title: "Indistinguishability Obfuscation",
+      description: "Resources about indistinguishability obfuscation (iO).",
+      emoji: "🔒",
+      links: [
+        {
+          title: "Diamond IO",
+          description: "A new approach to iO.",
+          link: "https://eprint.iacr.org/2025/236",
+          seriesLinks: [
+            {
+              title: "The first signs of Practical iO",
+              link: "https://machina-io.com/posts/hello_world_first.html",
+            },
+            {
+              title: "Diamond IO",
+              link: "https://github.com/MachinaIO/diamond-io",
+            },
+          ],
+        },
+      ],
+    },
   ],
 };
