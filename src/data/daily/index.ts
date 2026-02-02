4711c0df10b0b8509e921f4dfb8aca389d04ea50
Icer Liang <liangshuang@sutu.tech>
2026-02-02 22:53:12 +0800

    update daily

--- src/data/daily/index.ts
+++ src/data/daily/index.ts
@@ -12,6 +12,7 @@ import news2510 from './202510.json' with { type: 'json' }
 import news2511 from './202511.json' with { type: 'json' }
 import news2512 from './202512.json' with { type: 'json' }
 import news2601 from './202601.json' with { type: 'json' }
+import news2602 from './202602.json' with { type: 'json' }
 
 // Import avatars for each category
 import paperImg from "../../assets/hinta/paper.png";
@@ -58,6 +59,7 @@ export const newsData: ProcessedDailyNewsItem[] = [
   ...news2511,
   ...news2512,
   ...news2601,
+  ...news2602,
 ].map((item: DailyNewsItem): ProcessedDailyNewsItem => {
   const [year, month, day] = item.date.split('-');
   return {
